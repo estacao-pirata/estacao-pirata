37e805b98d90869e465ce71e14a02cd0f59eb882
Raphael Bertoche <bertocheraphael@gmail.com>
2023-06-17 03:54:30 -0300

    Seleção sortida de conteúdo nyano

---
+++ Content.Server/Nyanotrasen/EvilTwin/EvilTwinComponent.cs
@@ -0,0 +1,13 @@
+using Content.Server.Mind.Components;
+
+namespace Content.Server.EvilTwin
+{
+    [RegisterComponent]
+    public sealed class EvilTwinComponent : Component
+    {
+        public Mind.Mind? TwinMind;
+
+        [DataField("firstMindAdded")]
+        public bool FirstMindAdded = false;
+    }
+}
