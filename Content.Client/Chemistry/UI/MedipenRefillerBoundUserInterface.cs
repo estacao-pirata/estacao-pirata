a9def9b86154eaa7a188a229b864d4c668367e61
DoutorWhite <thedoctorwhite@gmail.com>
2024-01-22 06:46:47 -0300

    It's now possible to transfer reagents

--- Content.Client/Chemistry/UI/MedipenRefillerBoundUserInterface.cs
+++ Content.Client/Chemistry/UI/MedipenRefillerBoundUserInterface.cs
@@ -25,6 +25,8 @@ namespace Content.Client.Chemistry.UI
 
             _window.OpenCentered();
             _window.OnClose += Close;
+            _window.OnTransferButtonPressed += args => SendMessage(new MedipenRefillerTransferReagentMessage(args.Id, args.Value, args.IsBuffer));
+            _window.InputEjectButton.OnPressed += _ => SendMessage(new ItemSlotButtonPressedEvent(SharedMedipenRefiller.InputSlotName));
         }
 
         protected override void UpdateState(BoundUserInterfaceState state)
@@ -45,6 +47,7 @@ namespace Content.Client.Chemistry.UI
                     break;
             }
         }
+
         protected override void Dispose(bool disposing)
         {
             base.Dispose(disposing);
