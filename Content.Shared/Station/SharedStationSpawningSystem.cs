c41f62bbbe71e030c181bd310d068fb6cc5a2c7e
Daniela <daniela.paladinof@gmail.com>
2024-02-26 13:03:33 -0300

    Fix - EncryptionKey não sendo adicionada ao entrar na partida | Add - Traduções FTL

--- Content.Shared/Station/SharedStationSpawningSystem.cs
+++ Content.Shared/Station/SharedStationSpawningSystem.cs
@@ -31,6 +31,7 @@ public abstract class SharedStationSpawningSystem : EntitySystem
                     InventorySystem.TryEquip(entity, equipmentEntity, slot.Name, true, force:true);
                 }
             }
+            
         }
 
         if (!TryComp(entity, out HandsComponent? handsComponent))
