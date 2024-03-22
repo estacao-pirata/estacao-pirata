caa6a82351f18d01afc0cde8decf43b4123eb1ef
Diego Asprino <diegoasprino07@gmail.com>
2024-03-22 18:40:21 -0300

    ops adicionei aspid sem querer

--- Content.Benchmarks/MapLoadBenchmark.cs
+++ Content.Benchmarks/MapLoadBenchmark.cs
@@ -46,8 +46,7 @@ public class MapLoadBenchmark
         PoolManager.Shutdown();
     }
 
-
-    public static readonly string[] MapsSource = { "Empty", "Box", "Aspid", "Bagel", "Dev", "CentComm", "Atlas", "Core", "TestTeg", "Saltern", "Packed", "Omega", "Cluster", "Reach", "Gemini","Origin", "Meta", "Marathon", "Europa", "MeteorArena", "Fland", "Barratry" };
+    public static readonly string[] MapsSource = { "Empty", "Box", "Bagel", "Dev", "CentComm", "Atlas", "Core", "TestTeg", "Saltern", "Packed", "Omega", "Cluster", "Reach", "Gemini","Origin", "Meta", "Marathon", "Europa", "MeteorArena", "Fland", "Barratry" };
 
     [ParamsSource(nameof(MapsSource))]
     public string Map;
