bf63063217a4cfa72201641d405aa4f5918f677a
Bruno <bruno319124@gmail.com>
2024-09-24 18:47:56 -0300

    Mudanças no codigo para utilização de TimeSpan inves de hours

--- Content.Server/Weather/WeatherCycleComponent.cs
+++ Content.Server/Weather/WeatherCycleComponent.cs
@@ -9,19 +9,18 @@ namespace Content.Server.Weather
     {
         [ViewVariables(VVAccess.ReadWrite), DataField("isEnabled")]
         public bool IsEnabled = true;
+        [ViewVariables(VVAccess.ReadOnly), DataField("nextWeatherCyclePauseEnd")]
+        public TimeSpan NextWeatherCyclePauseEnd { get; set; }
+        [ViewVariables(VVAccess.ReadOnly), DataField("PauseDuration")]
+        public TimeSpan PauseDuration { get; set; }
         [ViewVariables(VVAccess.ReadOnly), DataField("weatherCicleDuration")]
-        public int WeatherCicleDuration = 9;
-        [ViewVariables(VVAccess.ReadWrite), DataField("weatherCicleStart")]
-        public int WeatherCicleStart = 19;
+        public TimeSpan WeatherCicleDuration = TimeSpan.FromMinutes(20);
+        [ViewVariables(VVAccess.ReadWrite), DataField("NextWeatherCycleStart")]
+        public TimeSpan NextWeatherCycleStart;
         [ViewVariables(VVAccess.ReadWrite), DataField("minDuration")]
-        public int MinDuration = 3;
+        public int MinDuration = 1;
         [ViewVariables(VVAccess.ReadWrite), DataField("maxDuration")]
-        public int MaxDuration = 8;
-        [ViewVariables(VVAccess.ReadWrite), DataField("minStartHour")]
-        public int MinStartHour = 1;
-        [ViewVariables(VVAccess.ReadWrite), DataField("maxStartHour")]
-        public int MaxStartHour = 23;
-        [ViewVariables(VVAccess.ReadWrite), DataField("weatherIds")]
+        public int MaxDuration = 5;
         public List<string> WeatherIds { get; set; } = new();
         public bool IsCycleActive { get; set; } = false;
     }
--- Content.Server/Weather/WeatherCycleSystem.cs
+++ Content.Server/Weather/WeatherCycleSystem.cs
@@ -19,9 +19,9 @@ namespace Content.Server.Time
         [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
         [Dependency] private readonly IEntityManager _entityManager = default!;
         [Dependency] private readonly IMapManager _mapManager = default!;
+        [Dependency] private readonly IGameTiming _timing = default!;
         [Dependency] private readonly IEntitySystemManager _entitySystem = default!;
         [Dependency] private readonly SharedWeatherSystem _weatherSystem = default!;
-        [Dependency] private readonly IGameTiming _timing = default!;
         private TimeSystem? _timeSystem;
         private int _currentHour;
         private double _deltaTime;
@@ -43,58 +43,36 @@ namespace Content.Server.Time
 
         private void SetRandomCycleValues(WeatherCycleComponent comp)
         {
-            int previousStart = comp.WeatherCicleStart;
-
-            comp.WeatherCicleDuration = _random.Next(comp.MinDuration, comp.MaxDuration);
-            int newStartHour = _random.Next(comp.MinStartHour, comp.MaxStartHour);
-
-            // Verifica se a nova hora de começo de um ciclo está em um intervalo de pelo menos 2 de diferença
-            if (Math.Abs(newStartHour - previousStart) < 3)
-            {
-                comp.WeatherCicleStart = newStartHour >= comp.MaxStartHour ? newStartHour - 1 : newStartHour + 1;
-            }
-            else
-            {
-                comp.WeatherCicleStart = newStartHour;
-            }
+            comp.WeatherCicleDuration = TimeSpan.FromMinutes(_random.Next(comp.MinDuration, comp.MaxDuration));
+            // Define o tempo de início do próximo ciclo
+            comp.NextWeatherCycleStart = _timing.CurTime + comp.WeatherCicleDuration;
+            // Define o intervalo de pausa aleatório entre 5 a 10 minutos
+            comp.PauseDuration = TimeSpan.FromMinutes(_random.Next(5, 10));
+            comp.NextWeatherCyclePauseEnd = comp.NextWeatherCycleStart + comp.PauseDuration;
         }
+
 
         public override void Update(float frameTime)
         {
             if (_deltaTime >= 1.0)
             {
                 _deltaTime = 0;
-                _currentHour = _timeSystem!.GetStationTime().Hours;
+
                 foreach (var comp in EntityQuery<WeatherCycleComponent>())
                 {
-                    if (comp.IsEnabled)
+                    if (_timing.CurTime >= comp.NextWeatherCycleStart && comp.IsCycleActive)
                     {
-                        // Calcula a hora de término do ciclo
-                        int endHour = (comp.WeatherCicleStart + comp.WeatherCicleDuration) % 24;
-                        bool isWithinCycle;
-
-                        // Verifica se está em ciclo
-                        if (comp.WeatherCicleStart + comp.WeatherCicleDuration < 24)
-                        {
-                            isWithinCycle = _currentHour >= comp.WeatherCicleStart && _currentHour < endHour;
-                        }
-                        else
-                        {
-                            isWithinCycle = _currentHour >= comp.WeatherCicleStart || _currentHour < endHour;
-                        }
-
-                        // Verifica se o ciclo está ativo e se está em ciclo (IsCycleActive é para evitar um loop infinito de trocas sem parar)
-                        if (!comp.IsCycleActive && isWithinCycle)
-                        {
-                            SetNewWeather(comp);
-                            comp.IsCycleActive = true;
-                        }
-                        else if (comp.IsCycleActive && !isWithinCycle)
-                        {
-                            SetNewWeather(comp, true);
-                            SetRandomCycleValues(comp);
-                            comp.IsCycleActive = false;
-                        }
+                        // Termina o ciclo de clima
+                        SetNewWeather(comp, true);
+                        comp.IsCycleActive = false;
+                        // Define os valores do próximo ciclo
+                        SetRandomCycleValues(comp);
+                    }
+                    else if (_timing.CurTime >= comp.NextWeatherCyclePauseEnd && !comp.IsCycleActive)
+                    {
+                        // Inicia o novo ciclo de clima após a pausa
+                        SetNewWeather(comp);
+                        comp.IsCycleActive = true;
                     }
                 }
             }
@@ -104,13 +82,13 @@ namespace Content.Server.Time
             }
         }
 
+
         private void SetNewWeather(WeatherCycleComponent comp, bool? clean = null)
         {
             if (comp.WeatherIds.Count == 0)
                 return;
 
             var curTime = _timing.CurTime;
-            var maxTime = TimeSpan.MaxValue;
 
             var mapId = comp.Owner.ToCoordinates().GetMapId(_entityManager);
 
@@ -124,12 +102,6 @@ namespace Content.Server.Time
             }
 
             var climaId = _random.Pick(comp.WeatherIds);
-
-            if (TryComp<WeatherComponent>(_mapManager.GetMapEntityId(mapId), out var weatherComp) &&
-                weatherComp.Weather.TryGetValue(climaId, out var existing))
-            {
-                maxTime = curTime - existing.StartTime;
-            }
 
             if (!_prototypeManager.TryIndex<WeatherPrototype>(climaId, out var clima))
                 return;
