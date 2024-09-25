d7b6658ee5f494ab4cddbd3f657f80c354974bcc
Nagel <pedronagelalves@gmail.com>
2024-09-25 16:19:59 -0300

    adição de novas funções de binding

--- Content.Client/RoundEnd/RoundEndSummaryUIController.cs
+++ Content.Client/RoundEnd/RoundEndSummaryUIController.cs
@@ -1,7 +1,6 @@
 using Content.Client.GameTicking.Managers;
 using Content.Shared.GameTicking;
 using Content.Shared.Input;
-using Content.Client.Options.UI.Tabs;
 using JetBrains.Annotations;
 using Robust.Client.Input;
 using Robust.Client.UserInterface.Controllers;
